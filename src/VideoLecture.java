99289d5e6fcfaea663757a30c9f96aa477905563
zhanboty08-afk <zhanboty08@gmail.com>
2025-12-30 17:20:16 +0500

    Refactor VideoLecture class to implement Downloadable

--- src/VideoLecture.java
+++ src/VideoLecture.java
@@ -1,4 +1,38 @@
-package PACKAGE_NAME;
+public class VideoLecture extends ContentItem implements Downloadable {
+    private String quality;
 
-public class VideoLecture {
+    public VideoLecture(String title, int year, int durationMinutes,String quality){
+        super(title, year, durationMinutes);
+        setQuality(quality);
+    }
+    public String getQuality() {
+        return quality;
+    }
+
+    private void setQuality(String quality) {
+        if (quality == null || quality.isBlank()) {
+            throw new IllegalArgumentException("Quality cannot be null or blank");
+        }
+        this.quality = quality;
+    }
+
+
+    public double getLicenseCost(int currentYear) {
+        int age = getAge(currentYear);
+        int ageFactor = (age <= 2) ? 5 : 2;
+        return 0.05 * durationMinutes + ageFactor;
+    }
+
+    public void download() {
+        System.out.println("Downloading video in " + quality + "...");
+    }
+
+    public int getMaxDownloadsPerDay() {
+        return 3;
+    }
+    public String toString() {
+        return "VideoLecture {" + super.toString() +
+                ", quality='" + quality + '\'' +
+                '}';
+    }
 }
