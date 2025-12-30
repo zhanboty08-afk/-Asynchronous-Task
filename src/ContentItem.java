c9283bc8506b6594218708cd6366b1029d57d8af
zhanboty08-afk <zhanboty08@gmail.com>
2025-12-30 17:18:51 +0500

    Refactor ContentItem class to be abstract

--- src/ContentItem.java
+++ src/ContentItem.java
@@ -1,4 +1,47 @@
-package PACKAGE_NAME;
+public abstract class ContentItem {
+    private int id;
+    private static int idGen;
+    private String title;
+    private int year;
+    public int durationMinutes;
 
-public class ContentItem {
+    public ContentItem(String title, int year, int durationMinutes){
+        this.id = idGen++;
+        setTitle(title);
+        setYear(year);
+        setduration(durationMinutes);
+    }
+    public int getId() {
+        return id;
+    }
+
+    private void setYear(int year) {
+        if (year < 1990 || year >2025)
+            throw new IllegalArgumentException("Invalid year");
+        this.year = year;
+    }
+
+    private void setTitle(String title) {
+        if (title == null || title.isEmpty())
+            throw new IllegalArgumentException("Title cannot be empty");
+        this.title = title;
+    }
+
+    private void setduration(int durationMinutes) {
+        if (durationMinutes<0)
+            throw new IllegalArgumentException("Invalid year");
+        this.durationMinutes = durationMinutes;
+    }
+
+    public int getAge(int currentYear) {
+        return currentYear - year;
+    }
+    public abstract double getLicenseCost(int currentYear);
+
+    public String toString(){
+        return "ID: " + id +
+                ", title: " + title +
+                ", Year: " + year +
+                ", duration: " + durationMinutes;
+    }
 }
