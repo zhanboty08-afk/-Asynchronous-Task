d8f3a062df593ae44bd30e814a89721c0f172ee4
zhanboty08-afk <zhanboty08@gmail.com>
2025-12-30 17:19:46 +0500

    Refactor PodcastEpisode class to implement Downloadable

--- src/PodcastEpisode.java
+++ src/PodcastEpisode.java
@@ -1,4 +1,39 @@
-package PACKAGE_NAME;
+public class PodcastEpisode extends ContentItem implements Downloadable{
 
-public class PodcastEpisode {
+    private String hostName;
+
+    public PodcastEpisode(String title, int year, int durationMinutes, String hostName) {
+        super(title, year, durationMinutes);
+        setHostName(hostName);
+    }
+
+    public String getHostName() {
+        return hostName;
+    }
+
+    public void setHostName(String hostName) {
+        if (hostName == null || hostName.isBlank()) {
+            throw new IllegalArgumentException("Host name cannot be null or blank");
+        }
+        this.hostName = hostName;
+    }
+
+    public double getLicenseCost(int currentYear) {
+        int age = getAge(currentYear);
+        int ageFactor = (age <= 2) ? 3 : 1;
+        return 0.03 * durationMinutes + ageFactor;
+    }
+    public void download() {
+        System.out.println("Downloading podcast hosted by " + hostName + "...");
+    }
+
+    public int getMaxDownloadsPerDay() {
+        return 10;
+    }
+
+    public String toString() {
+        return "PodcastEpisode {" + super.toString() +
+                ", host='" + hostName + '\'' +
+                '}';
+    }
 }
